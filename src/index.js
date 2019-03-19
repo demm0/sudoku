27f9a6046b0c6c86ddd52d1532ae2b99f327a8b6
demm0 <demm0@mail.ru>
2019-03-19 23:17:39 +0300

    edit index.js
    edit code

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 module.exports = function solveSudoku(matrix) {
+	
 	let m = [];
+	let usl;
+	
 	for (let i = 0; i < matrix.length; i ++){
 		m[i] = [];
 		for (let j = 0; j < matrix[i].length; j ++){
@@ -7,7 +10,9 @@ module.exports = function solveSudoku(matrix) {
 			m[i][j][0] = matrix[i][j];
 		}
 	}
+	
 	let matrix_9 = [];			  
+	
 	for (i = 0; i < m.length+1; i++){	 
 		matrix_9[i]=i;
 	}
@@ -34,6 +39,7 @@ module.exports = function solveSudoku(matrix) {
 			for (let s = 1; s < m[r][t].length; s ++){
 				if (m[r][t][s] == m[r][l][0]){		
 					m[r][t].splice (s,1);
+					usl ++;
 				}
 			}
 		}
@@ -51,6 +57,7 @@ module.exports = function solveSudoku(matrix) {
 			}
 			if (schet == 0) {
 				m[r][t] = [m[r][t][s]];
+				usl ++;
 				break;
 			}
 		}
@@ -61,23 +68,25 @@ module.exports = function solveSudoku(matrix) {
 			for (let s = 1; s < m[r][t].length; s ++){
 				if (m[r][t][s] == m[l][t][0]){
 					m[r][t].splice (s,1);
+					usl ++;
 				}
 			}
 		}
 	}	
 	
 	function incolumn ( r , t ) {																	// function incolumn
-		for (l = 1; l<m[r][t].length; l++){
+		for (s = 1; s < m[r][t].length; s ++){
 			let schet = 0;
 			for (let i = 0; i < m[r].length; i ++){
 				for (let k = 0; k < m[i][t].length; k ++){
-					if ((m[r][t][l] == m[i][t][k]) && (i != r)){		
-						schet++;
+					if ((m[r][t][s] == m[i][t][k]) && (i != r)){		
+						schet ++;
 					}
 				}
 			}
 			if (schet == 0) {
-				m[r][t] = [m[r][t][l]];
+				m[r][t] = [m[r][t][s]];
+				usl ++;
 				break;
 			}
 		}
@@ -92,6 +101,7 @@ module.exports = function solveSudoku(matrix) {
 					for (let s = 1; s < m[r][t].length; s ++){
 						if (m[r][t][s] == m[l][p][0]){
 							m[r][t].splice(s,1);
+							usl ++;
 						}
 					}
 				}
@@ -100,13 +110,14 @@ module.exports = function solveSudoku(matrix) {
 	}
 
 	function insector ( r , t ){																	// function insector
-		for (let s = 1; s<m[r][t].length; s++){
+		for (let s = 1; s < m[r][t].length; s ++){
 			let schet = 0;
-			for (let l = 1; l < m.length; l++){
+			for (let l = 0; l < m.length; l++){
 				for (let p = 0; p < m[l].length; p ++){
 					if ((Math.trunc(r / Math.sqrt(m.length)) == Math.trunc(l / Math.sqrt(m.length))) 
 				 	&& (Math.trunc(t / Math.sqrt(m.length)) == Math.trunc(p / Math.sqrt(m.length)))
-				 	&& ((l * 10 + p) != (r * 10 + t))){	
+//					  ((l * 10 + p) != (r * 10 + t))
+					 ){	
 						for (let k = 0; k < m[l][p].length; k ++){
 							if (m[r][t][s] == m[l][p][k]){		
 								schet++;
@@ -117,15 +128,16 @@ module.exports = function solveSudoku(matrix) {
 			}
 			if (schet == 0) {
 				m[r][t] = [m[r][t][l]];
+				usl ++;
 				break;	
 			}
 		}					
 	}
-
-let x = 1;
-
+	let usl2 = 0;
 	do{	
-		let usl = false;
+		usl2 = 0;
+	do{	
+		usl = 0;
 		for (i = 0; i < m.length; i ++){
 			for (j = 0; j < m[i].length; j ++){														
 				if (m[i][j].length > 2) {
@@ -134,23 +146,37 @@ let x = 1;
 					sector (i, j); 
 					inline (i, j);
 					incolumn (i, j);
-//					insector (i, j); 
-					usl = true;
+					insector (i, j); 
 				}
 				if (m[i][j].length == 2) {
 					m[i][j].splice (0,1);
 				}
 			}	
 		}
-		x++;
-	}	while (x <= 6);
+
+	}	while (usl != 0);
+	for (let i = 0; i < m.length; i ++) {
+		for (let j = 0; j < m[i].length; j ++){
+			if (m[i][j].length > 2) {
+				prom = m[i][j][1];
+				m[i][j] = [];
+				m[i][j].push(prom);
+				usl2 ++;
+				usl = 0;
+				break;
+			}
+		}
+		if (usl2 != 0){
+			break;
+		}
+	}
+} while (usl2 != 0)
 
 	for (let i = 0; i < m.length; i ++){
 		for (let j = 0; j < m[i].length; j ++){
 			matrix[i][j] = m[i][j][0]; 
 		}
 	}
-
 	return matrix;
   // your solution
 }
